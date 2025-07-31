5ad998e82f80733691d0936ae2e5db94ef93c64b
eric-gitta-moore <1462066778@qq.com>
2025-07-31 18:21:38 +0800

    update dep

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ const { defineConfig } = require("eslint-define-config");
 
 module.exports = defineConfig({
   root: true,
-  extends: ["plugin:vue/vue3-recommended"],
+  extends: ["plugin:vue/recommended"],
   parser: "vue-eslint-parser",
   parserOptions: {
     parser: "@typescript-eslint/parser",
